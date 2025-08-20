d1440aa56f5748a4e3618eb2c2006f7a2c94e18f
Saad <saadmuizz@gmail.com>
2025-08-19 21:06:37 -0400

    update

---
+++ collection/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+#vcpkg_from_github(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    REPO open-source-patterns/collection
+#    REF "${VERSION}"
+#    SHA512 cfa5f79d2935704a10a95b0f3ebacf73e500425ea38f55e0c93f3c59a8b7c336164f899b9462aa60c6c878589992efa5a3efb0d414cc0f357d7dc269cccb215f
+#    HEAD_REF main
+#)
+
+set(SOURCE_PATH "/Users/sshams/Documents/open-source-patterns/collection")
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "collection" CONFIG_PATH "lib/cmake/collection")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}") # usage
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
